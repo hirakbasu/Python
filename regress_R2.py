5c7555ba712826ac5c707b51e961fcdc2d17b24c
hirakbasu <123515403+hirakbasu@users.noreply.github.com>
2023-04-06 18:37:50 -0400

    Add files via upload

--- regress_R2.py
+++ regress_R2.py
@@ -1,20 +1,22 @@
 import warnings
 from scipy.stats import linregress
+import numpy as np
 
-def regress_R2(OTFshifted_OTFoverlap_ang, DO1overlap6_ang, DO0overlap6_ang, Progressbar):
+def regress_R2(OTFshifted_OTFoverlap_ang, DO1overlap6_ang, DO0overlap6_ang, Progressbar = False):
     # regress_R2(OTFshifted_OTFoverlap_ang(:,:,ii), DO1overlap6_ang(:,:,ii), DO0overlap6_ang(:,:,ii), Progressbar); 
     # check DO1overlap6_angles <-(OTFshifted_OTFoverlap_ang, DO1overlap6_ang)  
     warnings.filterwarnings('ignore', '.*NoConst*')
     R2_a_ang = OTFshifted_OTFoverlap_ang
-    R2_b_ang = R2_a_ang.reshape(-1, 1)
+    R2_b_ang = np.zeros((OTFshifted_OTFoverlap_ang.size, 3))
+    R2_b_ang[:, 0] = np.ravel(R2_a_ang, order='F').reshape(-1)
     R2_b_ang[R2_b_ang != 0] = 1
     
     R2_c = DO1overlap6_ang
-    R2_d = R2_c.reshape(-1, 1)
+    R2_d = np.ravel(R2_c, order='F').reshape(-1)
     R2_b_ang[:, 1] = R2_d
     
     R2_c = DO0overlap6_ang
-    R2_d = R2_c.reshape(-1, 1)
+    R2_d = np.ravel(R2_c, order='F').reshape(-1)
     R2_b_ang[:, 2] = R2_d
     
     R2_b_ang = R2_b_ang[(R2_b_ang[:, 0] != 0), :]  # mask with OTF overlap
