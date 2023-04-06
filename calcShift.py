bcbe35600aad173aeec7abde3c46bee399e1a782
hirakbasu <123515403+hirakbasu@users.noreply.github.com>
2023-04-05 21:03:40 -0400

    Add files via upload

--- calcShift.py
+++ calcShift.py
@@ -2,13 +2,13 @@ import numpy as np
 # Calculates the magnitude of SIM vector shift
 
 def calcShift(x, y):
-    x = x.reshape(3, 3) # Reshapes x and y into 3x3 matrix
-    y = y.reshape(3, 3)
-    x2 = x - x[0, :]    # Sets first row to 0s to apparently make calculations easier
-    y2 = y - y[0, :]
+    x = np.array(x).reshape(3, 3)   # Reshapes x and y from 9 column vector to 3x3 matrix
+    y = np.array(y).reshape(3, 3)
+    x2 = x - x[0]    # Sets first row to 0s to apparently make calculations easier
+    y2 = y - y[0]
     
-    r = np.sqrt(x2**2 + y2**2)  # Finds distance, then transforms back into column vector
-    r = r[1:, :].flatten()
+    r = np.sqrt(x2**2 + y2**2)  # Finds distance
+    r = r[1:,:].reshape(-1,1)   # Removes first row and reshpes into column vector
     
     cc = 3  # No idea what cc means here, probably useless
     return r
