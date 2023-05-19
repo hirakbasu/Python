20a225dbab26cc14c352eb37d2f7f8dd47621c2d
hirakbasu <123515403+hirakbasu@users.noreply.github.com>
2023-05-19 15:47:58 -0400

    Add files via upload

---
+++ dispSLMimg.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.ndimage
+import math
+
+def dispSLMimg(n, pxIx, th, isDisp, isDbg):
+    # n: # px
+    # pxIx: positive px
+    # rotation angle
+    
+    A = np.zeros(n**2)
+    for index in pxIx:
+        A[index - 1] = 1
+    square = int(math.sqrt(len(A)))
+    A = [[A[j+i*square] for i in range(square)] for j in range(square)]
+
+    # A = scipy.ndimage.rotate(A, th, reshape=True, order=0)
+    # Okay so now we're gonna rotate because scipy.ndimage.rotate doesn't work, so this is gonna be manually done
+    A1 = np.array(A)
+    center = np.array(A1.shape) // 2
+    angle = np.radians(th)
+    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+    coords = np.argwhere(A1 == 1)
+    translated_coords = coords - center
+    rotated_coords = np.dot(translated_coords, rotation_matrix)
+    restored_coords = (rotated_coords + center).astype(int)
+    rotated_size = np.max(np.abs(restored_coords), axis=0) * 2 + 1
+    rotated_size = np.ceil(rotated_size).astype(int)
+    rotated_matrix = np.zeros(rotated_size)
+    new_center = (rotated_size - 1) // 2
+    shifted_coords = restored_coords + new_center
+    rotated_matrix[shifted_coords[:, 0], shifted_coords[:, 1]] = 1
+    A = rotA
+    
+    if isDisp:
+        plt.imshow(A, cmap='gray')
+        plt.axis('off')
+        plt.show()
+    
+    if isDbg:
+        fig, ax = plt.subplots()
+        ax.imshow(A, cmap='binary')
+        ax.axis('off')
+        plt.show()
+    
+    return A
---
+++ dispSLMpattern.py
@@ -0,0 +1,32 @@
+import numpy as np
+import math
+
+def dispSLMpattern(a, n, pxSz, t, s, isDbg):
+    # a: pattern width
+    # n: # px
+    # t: tilt angle pi/rad
+    # s: shift
+
+    d0 = s * a * 2  # distance from (0,0)
+    if d0 > a:
+        d0 = d0 - 2 * a
+
+    # initialize
+    px = []
+    if isDbg:
+        px = initializeSLMpx(n, pxSz)
+
+    # switch pixels
+    tp = t * math.pi
+    np = math.ceil(((math.tan(tp) + 1) * n - d0 / math.cos(tp)) / (a / math.cos(tp)) / 2)  # # of stripes
+    pxIx = []
+
+    for i in range(1, np+1):  # each stripe
+        memoryLimitPause()
+        print(f"adding stripe: {i}//{np}")
+
+        d = d0 + 2 * a * (i - 1)
+        px, pxix = findPxSLM(px, a, n, pxSz, t, d, isDbg)  # find px in stripe
+        pxIx.append(pxix)
+        
+    return pxIx
